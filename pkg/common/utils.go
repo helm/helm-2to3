980c738ff22a8a035dce8ac6614f583f79db0113
Martin Hickey <martin.hickey@ie.ibm.com>
2019-12-09 08:20:53 +0000

    Fix plugin symbolic links (#100)
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>

--- pkg/common/utils.go
+++ pkg/common/utils.go
@@ -31,8 +31,6 @@ import (
 	v3 "github.com/helm/helm-2to3/pkg/v3"
 )
 
-const sep = string(filepath.Separator)
-
 // Copyv2HomeTov3 copies the v2 home directory to the v3 home directory .
 // Note that this is not a direct 1-1 copy
 func Copyv2HomeTov3(dryRun bool) error {
@@ -42,6 +40,8 @@ func Copyv2HomeTov3(dryRun bool) error {
 	log.Printf("[Helm 3] Config directory: %s\n", v3ConfigDir)
 	v3DataDir := v3.DataDir()
 	log.Printf("[Helm 3] Data directory: %s\n", v3DataDir)
+	v3CacheDir := v3.CacheDir()
+	log.Printf("[Helm 3] Cache directory: %s\n", v3CacheDir)
 
 	// Create Helm v3 config directory if needed
 	log.Printf("[Helm 3] Create config folder \"%s\" .\n", v3ConfigDir)
@@ -55,8 +55,8 @@ func Copyv2HomeTov3(dryRun bool) error {
 	}
 
 	// Move repo config
-	v2RepoConfig := v2HomeDir + sep + "repository" + sep + "repositories.yaml"
-	v3RepoConfig := v3ConfigDir + sep + "repositories.yaml"
+	v2RepoConfig := filepath.Join(v2HomeDir, "repository", "repositories.yaml")
+	v3RepoConfig := filepath.Join(v3ConfigDir, "repositories.yaml")
 	log.Printf("[Helm 2] repositories file \"%s\" will copy to [Helm 3] config folder \"%s\" .\n", v2RepoConfig, v3RepoConfig)
 	if !dryRun {
 		err = copyFile(v2RepoConfig, v3RepoConfig)
@@ -66,10 +66,17 @@ func Copyv2HomeTov3(dryRun bool) error {
 		log.Printf("[Helm 2] repositories file \"%s\" copied successfully to [Helm 3] config folder \"%s\" .\n", v2RepoConfig, v3RepoConfig)
 	}
 
-	// Not moving local repo as it is safer to recreate: e.g. v2HomeDir/repository/local
+	// Not moving local repo and its cache, as it is safer to recreate: e.g. v2HomeDir/repository/local v2HomeDir/repository/cache
 
-	// Not moving the cache as it is safer to recreate when needed
-	// v2HomeDir/cache and v2HomeDir/repository/cache
+	// Create Helm v3 cache directory if needed
+	log.Printf("[Helm 3] Create cache folder \"%s\" .\n", v3CacheDir)
+	if !dryRun {
+		err = ensureDir(v3CacheDir)
+		if err != nil {
+			return fmt.Errorf("[Helm 3] Failed to create cache folder \"%s\" due to the following error: %s", v3CacheDir, err)
+		}
+		log.Printf("[Helm 3] cache folder \"%s\" created.\n", v3CacheDir)
+	}
 
 	// Create Helm v3 data directory if needed
 	log.Printf("[Helm 3] Create data folder \"%s\" .\n", v3DataDir)
@@ -81,21 +88,37 @@ func Copyv2HomeTov3(dryRun bool) error {
 		log.Printf("[Helm 3] data folder \"%s\" created.\n", v3DataDir)
 	}
 
-	// Move plugins
-	v2Plugins := v2HomeDir + sep + "plugins"
-	v3Plugins := v3DataDir + sep + "plugins"
-	log.Printf("[Helm 2] plugins \"%s\" will copy to [Helm 3] data folder \"%s\" .\n", v2Plugins, v3Plugins)
-	if !dryRun {
-		err = copyDir(v2Plugins, v3Plugins)
-		if err != nil {
-			return fmt.Errorf("Failed to copy [Helm 2] plugins directory \"%s\" due to the following error: %s", v2Plugins, err)
+	// Handle plugins
+	v2Plugins := filepath.Join(v2HomeDir, "cache", "plugins")
+	plugins, _ := pathExists(v2Plugins)
+	if plugins {
+		// Move plugins
+		v2Plugins := filepath.Join(v2HomeDir, "cache", "plugins")
+		v3Plugins := filepath.Join(v3CacheDir, "plugins")
+		log.Printf("[Helm 2] plugins \"%s\" will copy to [Helm 3] cache folder \"%s\" .\n", v2Plugins, v3Plugins)
+		if !dryRun {
+			err = copyDir(v2Plugins, v3Plugins)
+			if err != nil {
+				return fmt.Errorf("Failed to copy [Helm 2] plugins directory \"%s\" due to the following error: %s", v2Plugins, err)
+			}
+			log.Printf("[Helm 2] plugins \"%s\" copied successfully to [Helm 3] cache folder \"%s\" .\n", v2Plugins, v3Plugins)
 		}
-		log.Printf("[Helm 2] plugins \"%s\" copied successfully to [Helm 3] data folder \"%s\" .\n", v2Plugins, v3Plugins)
+
+		// Recreate the  plugin symbolic links for v3 path
+		v2Links := filepath.Join(v2HomeDir, "plugins")
+		log.Printf("[Helm 2] plugin symbolic links \"%s\" will copy to [Helm 3] data folder \"%s\" .\n", v2Links, v3DataDir)
+		if !dryRun {
+			err = reCreatePluginSymLinks(v2Links, v3DataDir, v3CacheDir)
+			if err != nil {
+				return fmt.Errorf("Failed to copy [Helm 2] plugin links \"%s\" due to the following error: %s", v2Links, err)
+			}
+			log.Printf("[Helm 2] plugin links \"%s\" copied successfully to [Helm 3] data folder \"%s\" .\n", v2Links, v3DataDir)
+		}
 	}
 
 	// Move starters
-	v2Starters := v2HomeDir + sep + "starters"
-	v3Starters := v3DataDir + sep + "starters"
+	v2Starters := filepath.Join(v2HomeDir, "starters")
+	v3Starters := filepath.Join(v3DataDir, "starters")
 	log.Printf("[Helm 2] starters \"%s\" will copy to [Helm 3] data folder \"%s\" .\n", v2Starters, v3Starters)
 	if !dryRun {
 		err = copyDir(v2Starters, v3Starters)
@@ -145,9 +168,12 @@ func copyDir(srcDirName, destDirName string) error {
 
 	directory, _ := os.Open(srcDirName)
 	objects, err := directory.Readdir(-1)
+	if err != nil {
+		return fmt.Errorf("Failed to copy directory  due to the following error: %s", err)
+	}
 	for _, obj := range objects {
-		srcFileName := srcDirName + sep + obj.Name()
-		destFileName := destDirName + sep + obj.Name()
+		srcFileName := filepath.Join(srcDirName, obj.Name())
+		destFileName := filepath.Join(destDirName, obj.Name())
 		if obj.IsDir() {
 			// create sub-directories - recursively
 			err = copyDir(srcFileName, destFileName)
@@ -184,15 +210,62 @@ func ensureDir(dirName string) error {
 	return nil
 }
 
+func pathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
+}
+
 func copySymLink(fileInfo os.FileInfo, srcDirName, destDirName string) error {
-	originFileName, err := os.Readlink(srcDirName + sep + fileInfo.Name())
+	originFileName, err := os.Readlink(filepath.Join(srcDirName, fileInfo.Name()))
 	if err != nil {
 		return err
 	}
-	newSymLinkName := destDirName + sep + fileInfo.Name()
+	newSymLinkName := filepath.Join(destDirName, fileInfo.Name())
 	err = os.Symlink(originFileName, newSymLinkName)
 	if err != nil && !os.IsExist(err) {
 		return err
+	}
+	return nil
+}
+
+func reCreatePluginSymLinks(srcDirName, v3DataDir, v3CacheDir string) error {
+	v3PluginDataDir := filepath.Join(v3DataDir, "plugins")
+	err := ensureDir(v3PluginDataDir)
+	if err != nil {
+		return fmt.Errorf("Failed to create folder \"%s\" due to the following error: %s", v3PluginDataDir, err)
+	}
+	directory, _ := os.Open(srcDirName)
+	objects, err := directory.Readdir(-1)
+	if err != nil {
+		return fmt.Errorf("Failed to re-create symlinks due to the following error: %s", err)
+	}
+	for _, obj := range objects {
+		srcFileName := filepath.Join(srcDirName, obj.Name())
+		if !obj.IsDir() {
+			fileInfo, err := os.Lstat(srcFileName)
+			if err != nil {
+				return fmt.Errorf("Failed to check file \"%s\" stats  due to the following error: %s", srcFileName, err)
+			}
+			if fileInfo.Mode()&os.ModeSymlink != 0 {
+				symLinkName := obj.Name()
+				newFullSymLinkName := filepath.Join(v3PluginDataDir, symLinkName)
+				origFullFileName, err := os.Readlink(filepath.Join(srcDirName, fileInfo.Name()))
+				if err != nil {
+					return fmt.Errorf("Failed to re-create symlink for \"%s\" due to the following error: %s", symLinkName, err)
+				}
+				newFullFileName := filepath.Join(v3CacheDir, "plugins", filepath.Base(origFullFileName))
+				err = os.Symlink(newFullFileName, newFullSymLinkName)
+				if err != nil && !os.IsExist(err) {
+					return fmt.Errorf("Failed to re-create symlink for \"%s\" due to the following error: %s", newFullSymLinkName, err)
+				}
+			}
+		}
 	}
 	return nil
 }
