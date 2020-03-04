7a3b7cad652080bb5bb45aad2a3f08d7fd939dfa
Marc Khouzam <marc.khouzam@gmail.com>
2020-03-04 17:05:02 -0500

    Package new completion.yaml file
    Signed-off-by: Marc Khouzam <marc.khouzam@gmail.com>

--- scripts/install_plugin.sh
+++ scripts/install_plugin.sh
@@ -37,3 +37,4 @@ fi
 tar xzf "releases/v${version}.tar.gz" -C "releases/v${version}"
 mv "releases/v${version}/2to3" "bin/2to3" || \
     mv "releases/v${version}/2to3.exe" "bin/2to3"
+mv "releases/v${version}/completion.yaml" .
