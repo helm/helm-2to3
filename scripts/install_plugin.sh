b8e42f9f4c7de008303fe113cfa7ea18b3ed631e
rimas <rmocius@gmail.com>
2020-03-10 14:56:25 +0200

    Use version for all files in install_plugin.sh to archive
    Signed-off-by: rimas <rmocius@gmail.com>

--- scripts/install_plugin.sh
+++ scripts/install_plugin.sh
@@ -38,3 +38,6 @@ tar xzf "releases/v${version}.tar.gz" -C "releases/v${version}"
 mv "releases/v${version}/2to3" "bin/2to3" || \
     mv "releases/v${version}/2to3.exe" "bin/2to3"
 mv "releases/v${version}/completion.yaml" .
+mv "releases/v${version}/plugin.yaml" .
+mv "releases/v${version}/README.md" .
+mv "releases/v${version}/LICENSE" .
